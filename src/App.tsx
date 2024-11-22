d086fe0424aa994c349d11a7771612e232f91c4d
Gabriela Menezes <gabimenezess10@gmail.com>
2024-11-22 19:54:09 -0300

    feat: add taskInput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styles from './App.module.css'
 import { Header } from './components/Header'
+import { TaskInput } from './components/TaskInput'
 
 import './global.css'
 
@@ -7,8 +8,9 @@ function App() {
 
   return (
     <>
-        <Header />
+      <Header />
       <div className={styles.wrapper}>
+        <TaskInput />
       </div>
     </>
   )
---
+++ src/components/TaskInput.tsx
@@ -0,0 +1,13 @@
+import { PlusCircle } from '@phosphor-icons/react'
+import styles from './TaskInput.module.css'
+export const TaskInput = () => {
+  return (
+    <label className={styles.taskInput}>
+        <input type="text" placeholder='Adicione uma nova tarefa' />
+        <button>
+            Criar
+            <PlusCircle size={24}/>
+        </button>
+    </label>
+  )
+}
