0e94b0d0f06a2cf09786540808b2c7eab867c1e6
Gabriela Menezes <gabimenezess10@gmail.com>
2024-11-25 19:56:28 -0300

    refactor: moving handleCreateNewTask to App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,35 @@ import { TaskList } from './components/TaskList'
 
 import './global.css'
 import { TaskType } from './components/Task'
+import { PlusCircle } from '@phosphor-icons/react'
+import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
   const [taskInput, setTaskInput] = useState('');
-  const [taskList, setTaskList] = useState<TaskType[]>([]);
-  const [taskCount, setTaskCount] = useState(0);
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+
+  function handleCreateNewTask() {
+    setTasks([...tasks, {id: uuidv4(), name: taskInput, completed: false}])
+    setTaskInput('');
+  }
   return (
     <>
       <Header />
 
       <div className={styles.wrapper}>
-        <TaskInput 
-          taskInput={taskInput}
-          setTaskList={setTaskList} 
-          setTaskInput={setTaskInput} 
-          taskList={taskList} 
-          setTaskCount={setTaskCount}
-          taskCount={taskCount}
-          
-        />
+        <label>
+          <TaskInput 
+            taskInput={taskInput}
+            setTaskInput={setTaskInput} 
+            
+          />
+        </label>
+        
+        <button onClick={handleCreateNewTask}>
+            Criar
+            <PlusCircle size={22}/>
+        </button>
         <TaskList taskList={taskList} taskCount={taskCount}/>
       </div>
     </>
--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,36 +1,20 @@
-import { PlusCircle } from '@phosphor-icons/react'
-import { v4 as uuidv4 } from 'uuid';
 import styles from './TaskInput.module.css'
 import { ChangeEvent} from 'react';
-import { TaskType } from './Task';
 
 interface TaskInputProps {
   taskInput: string;
-  taskList: TaskType[];
-  taskCount: number;
-  setTaskCount: (value: number) => void;
-  setTaskList: (task: TaskType[]) => void;
   setTaskInput: (value: string) => void;
 }
-export const TaskInput = ({taskInput, setTaskList, taskList, setTaskInput, taskCount, setTaskCount} : TaskInputProps) => {
+export const TaskInput = ({taskInput, setTaskInput} : TaskInputProps) => {
 
   function handleCreateTaskInputChange(event: ChangeEvent<HTMLInputElement>) {
     const newTask = event.target.value;
     setTaskInput(newTask)
   }
 
-  function handleCreateNewTask() {
-    setTaskList([...taskList, {id: uuidv4(), name: taskInput, completed: false}])
-    setTaskCount(taskCount + 1)
-    setTaskInput('');
-  }
+ 
   return (
-    <label className={styles.taskInput}>
-        <input type="text" placeholder='Adicione uma nova tarefa' value={taskInput} onChange={(event) => handleCreateTaskInputChange(event)}/>
-        <button onClick={handleCreateNewTask}>
-            Criar
-            <PlusCircle size={22}/>
-        </button>
-    </label>
+      <input type="text" placeholder='Adicione uma nova tarefa' value={taskInput} onChange={(event) => handleCreateTaskInputChange(event)}/>
+
   )
 }
